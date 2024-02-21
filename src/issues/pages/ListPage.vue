20b6e066f64e92d783b34462617e72e93541c455
FedericoL_exo <federicol@exo.com.ar>
2024-02-21 15:50:44 -0300

    feat(final commit):Last commit where the queries are inserted to create an issue and the UI is updated in real time.

--- src/issues/pages/ListPage.vue
+++ src/issues/pages/ListPage.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
 import FilterSelector from 'src/issues/components/filter-selector/FilterSelector.vue';
 import IssueList from '../components/issue-list/IssueList.vue';
 import useIssues from '../composables/useIssues';
+import useLabels from '../composables/useLabels';
+import FloatingButtons from 'src/issues/components/FloatingButtons.vue';
+import NewIssueDialog from 'src/issues/components/NewIssueDialog.vue';
 
 const { issuesQuery } = useIssues();
+const { labelsQuery } = useLabels();
+
+const isOpen = ref<boolean>(false);
+
+const openDialog = () => {
+  isOpen.value = true;
+};
 </script>
 
 <template>
@@ -26,6 +37,25 @@ const { issuesQuery } = useIssues();
       <IssueList v-else :issues="issuesQuery.data?.value || []" />
     </div>
   </div>
+
+  <!-- FloatingButtons -->
+  <FloatingButtons
+    :buttons="[
+      {
+        icon: 'add',
+        color: 'primary',
+        size: 'lg',
+        action: openDialog,
+      },
+    ]"
+  />
+  <!-- Dialog Issue -->
+  <NewIssueDialog
+    v-if="labelsQuery.data"
+    :is-opened="isOpen"
+    :get-labels="labelsQuery.data.value?.map((label) => label.name) || []"
+    @on-closed="isOpen = false"
+  />
 </template>
 
 <style scoped></style>
